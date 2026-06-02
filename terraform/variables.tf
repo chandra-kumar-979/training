f0a014c46bf60391e5d7c16bab5ebc851a58dd82
chandrakumar <chandra.k5680@gmail.com>
2026-06-02 18:23:02 +0530

    terraform setup

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -19,11 +19,11 @@ variable "environment" {
 variable "cidr_block" {
   description = "The CIDR block for the VPC"
   type        = string
-  default     = "10.0.0.0/16"
+  default     = "10.0.0.0/21"
 }
 
 variable "subnet_cidr_block" {
   description = "The CIDR block for the subnet"
   type        = string
-  default     = "10.0.1.0/24"
+  default     = "10.0.1.0/28"
 }
